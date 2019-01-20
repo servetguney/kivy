79d4314a2d0e7d9f6c2a21dee6cdcff9e900c3c4
sguney <servetguney@gmail.com>
2019-01-20 09:54:26 +0300

    Main -  Commit - 2

--- t.py
+++ t.py
@@ -8,6 +8,10 @@ def timelevel(a: int, b: list):
     print("The a {}".format(a))
     print("The b {}".format(b))
 
+def level2(Address:str, doornumber:str):
+    print("The Address {}".format(Address))
+    print("The DoorNumber {}".format(doornumber))
+
 if __name__ == "__main__":
     b=[4,5]
     print("The name is {} {}".format(__name__,timelevel(5, b)))
